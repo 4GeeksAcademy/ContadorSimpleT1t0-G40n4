13c2b8284a59286340ae1fea309bca1302e9beac
T1t0-G40n4 <titogaona84@gmail.com>
2025-04-21 20:41:06 +0000

    Contador-Simple-T1t0g40n4

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Digit } from "./digit";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [timer, setTimer] = useState(0);
+	const [active, setActive] = useState(true);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	useEffect(() => {
+		if(active){
+			setTimeout(() => {
+				setTimer(value => value + 1)
+			}, 1000)
+		}
+
+	}, [timer, active])
+
+	const startStop = ()=>{
+		setActive(value => !value)
+	}
+	
+		const resetTimer =() =>{
+			setTimer(value => 0);
+		}
+
+	return (
+		<div className="text-center mt-4">
+			<div className="counter">
+				<Digit className="digit" number={<span className="fa-solid fa-clock" ></span>} />
+				<Digit className="digit" number={Math.floor(timer / 100000) % 10} />
+				<Digit className="digit" number={Math.floor(timer / 10000) % 10} />
+				<Digit className="digit" number={Math.floor(timer / 1000) % 10} />
+				<Digit className="digit" number={Math.floor(timer / 100) % 10} />
+				<Digit className="digit" number={Math.floor(timer / 10) % 10} />
+				<Digit className="digit" number={Math.floor(timer % 10)} />
+			</div>
+			<div className="mt-2">
+				<button onClick={startStop} className="mx-1 btn btn-success">Start</button>
+				<button onClick={startStop} className="mx1 btn btn-secondary">Stop</button>
+				<button onClick={resetTimer} className="mx-1 btn btn-danger">Retset</button>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/components/digit.jsx
@@ -0,0 +1,1 @@
+export const Digit = ({number})=> <div className="digit">{number}</div>
